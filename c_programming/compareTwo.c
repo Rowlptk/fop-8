3b0edd12b39864fe173c4f293a8e22eb709cc62b
Rahul Pathak <rahulzha93@gmail.com>
2019-12-05 13:07:44 +0545

    Operators

---
+++ c_programming/compareTwo.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+void main()
+{
+    int n1,n2,n3;
+    printf("Enter Three Numbers: ");
+    scanf("%d%d%d",&n1,&n2,&n3);
+    (n1>n2 && n1>n3)?printf("n1 = %d greater",n1):((n2>n3)?printf("n2 = %d is greater",n2):printf("n3 = %d is greater",n3));
+}
+
+compare two numbers ==>
+compare three numbers ==>
+check even or odd ==>
+
+thursday ==> conditionals ==> if, if..else, if..else if..else, nested if, switch.. case;
+
+==> leap year;
+==> 80-100 ==> A grade,60-79 ==> B grade, 40-59 ==> C grade , 0-39 ==> fail, 0-,100+ ==> Invalid marks
+
+1 week ==> next thursday
+// date validation
+==> 2019-02-29
+==> 2020-02-29
+==> 2019-12-31
+==> 2019-04-31
+
+
+
---
+++ c_programming/ternary.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+void main()
+{
+    int num;
+    printf("Enter a number : ");
+    scanf("%d",&num);
+    (num>0)?printf("Positive"):
+        ((num<0)?printf("Negative"):printf("Zero"));
+}
+
+compare two numbers ==>
+compare three numbers ==>
+check even or odd ==>
